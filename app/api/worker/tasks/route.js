599c0862f0789e457f6b78654d9a06cb9963f6b2
TarunpreetSingh23 <tarunpreets29@gmail.com>
2025-10-15 11:57:17 +0530

    Update route.js

--- app/api/worker/tasks/route.js
+++ app/api/worker/tasks/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import connectDB from "@/lib/db";
+import connectDB from "@/lib/mongodb";
 import Task from "@/models/Task";
 import Worker from "@/models/Worker";
 
